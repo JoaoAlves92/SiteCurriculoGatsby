dbaa2872e17ceb4110223b84dd2472155f5c4259
JoaoAlves92 <jvamarangonia@gmail.com>
2022-01-02 15:00:52 -0300

    adding back-to-top

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "../reset.css";
 import "../global.css";
+
+// Libs
+import BackToTop from 'react-back-to-top';
+import { FaArrowUp } from 'react-icons/fa';
+
+// Componentes
 import Header from "../components/Header";
 import Banner from "../components/Banner";
 import AboutMe from "../components/AboutMe";
@@ -13,6 +19,23 @@ const IndexPage = () => {
       <Banner/>
       <AboutMe/>
       <Skills/>
+      <BackToTop
+        mainStyle={{
+            width:'100%',
+            height:'100%',
+            marginTop: '20px'
+        }}
+        percentStyle={{
+            display: 'none'
+        }}
+        animate='rotate'
+        offsetTop={0}
+        //step={50}
+        //percent={true}
+        //visiblePercent={50}
+      >
+        <FaArrowUp size={26} />
+      </BackToTop>
     </main>
   )
 }
